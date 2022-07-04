f3781fea370d7acf6806dbbb0518c22f55e7ff5e
Sekwah <contact@sekwah.com>
2022-07-04 23:22:19 +0100

    feat: custom messages relating to initial join

--- src/main/java/com/clasher113/autoreconnect/DisconnectedScreen.java
+++ src/main/java/com/clasher113/autoreconnect/DisconnectedScreen.java
@@ -4,10 +4,7 @@ import java.lang.reflect.Field;
 import java.util.Iterator;
 import java.util.List;
 
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiDisconnected;
-import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.*;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.TextComponentBase;
@@ -44,15 +41,23 @@ public class DisconnectedScreen extends GuiScreen{
 		MainClass.isEnabled = false;
 		this.buttonList.clear();
 		//this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height / 4 + 120, I18n.format("gui.toMenu")));
-		//this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 162, I18n.format("gui.button.cancel0")));
-		this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height / 4 + 141, I18n.format("gui.button.reconnect") + MainClass.reconnectDelay + "..."));
+		this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height / 4 + 162, I18n.format("gui.button.cancel0")));
+		this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 141, I18n.format("gui.button.reconnect") + MainClass.reconnectDelay + "..."));
 		this.list = this.fontRenderer.listFormattedStringToWidth(errorDetail.getFormattedText(), this.width - 50);
 	}
 	protected void actionPerformed(GuiButton button) {
-		if (button.id == 0){
+		if (button.id == 1){
 			MainClass.onGuiDisconnected = false;
 			MainClass.syncConfig();
 			FMLClientHandler.instance().connectToServer(new GuiMainMenu(), new ServerData("server", OnJoinServerEvent.host, false));
+		}
+		else if (button.id == 0) {
+			MainClass.onGuiDisconnected = false;
+			MainClass.syncConfig();
+			MainClass.tryNumber = 0;
+			OnJoinServerEvent.host = null;
+			GuiMainMenu mainMenu = new GuiMainMenu();
+			mc.displayGuiScreen(mainMenu);
 		}
 	}
 	@SuppressWarnings("rawtypes")
@@ -65,17 +70,27 @@ public class DisconnectedScreen extends GuiScreen{
 		if(MainClass.reconnectDelayUpdater <= 0){
 			MainClass.reconnectDelay -=1;
 			MainClass.reconnectDelayUpdater = 20;
-			this.buttonList.remove(0);
-			this.buttonList.add(new GuiButton(0, this.width /2 - 100, this.height / 4 + 141, I18n.format("gui.button.reconnect") + MainClass.reconnectDelay + "..."));
+			this.buttonList.remove(1);
+			this.buttonList.add(new GuiButton(1, this.width /2 - 100, this.height / 4 + 141, I18n.format("gui.button.reconnect") + MainClass.reconnectDelay + "..."));
 		}
 		this.drawDefaultBackground();
-		this.drawCenteredString(this.fontRenderer, this.errorMessage, this.width / 2, this.height / 2 - 50, 11184810);
+
+		String reason = list.size() > 0 ? list.get(0).toString() : "";
+
+		boolean showCustomMessage = this.errorMessage.contains("Connection Lost") && reason.contains("Unable to connect you to");
+
+		this.drawCenteredString(this.fontRenderer, showCustomMessage ? I18n.format("reconnect.reason.title") : this.errorMessage, this.width / 2, this.height / 2 - 50, 11184810);
 		this.drawCenteredString(this.fontRenderer, I18n.format("gui.string.attemp") + MainClass.tryNumber, this.width / 2, 10, 11184810);
 		int k = this.height / 2 - 30;
-		if (list != null) {
-			for (Iterator iterator = list.iterator(); iterator.hasNext(); k += this.fontRenderer.FONT_HEIGHT) {
-				String s = (String)iterator.next();
-				this.drawCenteredString(this.fontRenderer, s, this.width / 2, k, 16777215);
+
+		if(showCustomMessage) {
+			this.drawCenteredString(this.fontRenderer, I18n.format("reconnect.reason.message"), this.width / 2, k, 16777215);
+		} else {
+			if (list != null) {
+				for (Iterator iterator = list.iterator(); iterator.hasNext(); k += this.fontRenderer.FONT_HEIGHT) {
+					String s = (String)iterator.next();
+					this.drawCenteredString(this.fontRenderer, s, this.width / 2, k, 16777215);
+				}
 			}
 		}
 		super.drawScreen(par1, par2, par3);
